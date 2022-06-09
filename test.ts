2c14e8c60335aa3b49895f65d971009e5821aab4
Maurice de Beijer <maurice.de.beijer@gmail.com>
2022-06-09 15:44:13 +0200

    Working

---
+++ test.ts
@@ -0,0 +1,13 @@
+// import 'reflect-metadata';
+import * as d3 from 'd3';
+// console.log(d3.area.toString());
+
+// const d3 = require('d3');
+
+async function test() {
+  // await import('reflect-metadata');
+  //   const d3 = await import('d3');
+  console.log(d3.area.toString());
+}
+
+test().then(() => console.log('All done'));
