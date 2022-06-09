923afa9e79ca629c9b5872c66146f5c43b63803c
Maurice de Beijer <maurice.de.beijer@gmail.com>
2022-06-09 16:50:52 +0200

    With nodemon

--- test.ts
+++ test.ts
@@ -1,13 +1,3 @@
-// import 'reflect-metadata';
+import 'reflect-metadata';
 import * as d3 from 'd3';
-// console.log(d3.area.toString());
-
-// const d3 = require('d3');
-
-async function test() {
-  // await import('reflect-metadata');
-  //   const d3 = await import('d3');
-  console.log(d3.area.toString());
-}
-
-test().then(() => console.log('All done'));
+console.log(d3.area.toString());
